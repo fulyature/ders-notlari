e066f4d2fe2af64d1d9ed6b2b039cccfa4851d88
fulya_ture <fulyature@gmail.com>
2024-07-04 20:18:20 +0300

    learning

--- js-3-condionals/app2.js
+++ js-3-condionals/app2.js
@@ -2,7 +2,7 @@
 // *               KARAR YAPILARI
 // * ============================================
 
-console.log("*** CONDITIONS ***");
+// console.log("*** CONDITIONS ***");
 
 //! ************ IF - ELSE
 
@@ -16,8 +16,8 @@ else{
     şart durumu doğru değilse bu blok çalıştırılır.
 }
 */
-let sayi = 67;
-let kalan = sayi % 2;
+// let sayi = 67;
+// let kalan = sayi % 2;
 // console.log(kalan);
 
 // if (kalan === 0) {
@@ -33,15 +33,87 @@ let kalan = sayi % 2;
 // {
 //   console.log("yanmaz");
 // }
+//
+//
+//
+//
+// let num = +prompt("sayı giriniz");
 
-let num = +prompt("sayı giriniz");
+// if (num > 0) {
+//   console.log("sayı pozitif");
+// } else if (num < 0) {
+//   console.log("sayı negatif");
+// } else if (num === 0) {
+//   console.log("sayı sıfır");
+// } else {
+//   console.log("sadece sayı giriniz");
+// }
+//
+//
+//
+// verilen en büyügünü bul
+// let sayi1 = 150;
+// let sayi2 = 25;
+// let sayi3 = 30;
+// if (sayi1 > sayi2 && sayi1 > sayi3) {
+//   console.log("en büyük sayı: sayi1");
+// } else if (sayi2 > sayi3 && sayi2 > sayi1) {
+//   console.log("en büyük sayı: 2");
+// } else {
+//   console.log("en büyük sayı: 3");
+// }
 
-if (num > 0) {
-  console.log("sayı pozitif");
-} else if (num < 0) {
-  console.log("sayı negatif");
-} else if (num === 0) {
-  console.log("sayı sıfır");
-} else {
-  console.log("sayı giriniz");
-}
+//js deki built in fks kullanımı
+// const enb = Math.max(sayi1, sayi2, sayi3);
+// console.log(enb);
+
+//yaşı 18 den buy. oy kullanır, değilse kullanamaz
+
+//let yas = 20;
+// if(yas>18){
+//   console.log("oy kullnır");
+// }else{
+//   console.log("kullanamaz");
+// }
+
+//? ternary
+//1.
+// yas > 18 ? console.log("oy kullanabılır") : console.log("oy kullanamaz");
+// yas > 18 ? console.log("oy kullanabılır") : null;
+
+//2.
+// const message = yas > 18 ? "oy kullan" : "oy kullanma";
+// message === "oy kullan"
+//   ? console.log("seçim sandığına git")
+//   : console.log("evde otur");
+
+// const sayi = 20;
+// sayi % 2 === 0 ? console.log("sayi çift") : console.log("sayi  tek");
+
+//nested thernary
+//eğer 14 den kucukse CSSLayerBlockRule, 14-18 arası ergen,18 ustu yetısgın
+
+// const yas = 19;
+// const sonuc =
+//   yas <= 14
+//     ? console.log("çocuk")
+//     : yas <= 18
+//     ? console.log("ergen")
+//     : console.log("yetısgın");
+
+// let num1 = -10;
+// const sonuc =
+//   num1 > 0 ? "sayı pozitıf" : num1 < 0 ? "sayı negatif" : "sayı 0 dır";
+// console.log(sonuc);
+
+// let sayi = 5;
+// const sonuc = sayi < 0 ? "negatif" : sayi > 0 ? "pozitif" : "notr";
+// console.log(sonuc);
+//
+//
+//SHORT CIRCUT
+
+// let maas = 40000;
+// let yil = 5;
+// maas=maas+maas*0.5
+// yil > 10 && (maas =  maas * 1.5) : null
---
+++ teamwork-3-2/index.js
@@ -0,0 +1,11 @@
+let i = 0;
+
+while (i < 10) {
+  if (i === 3) {
+    break;
+  }
+  console.log(i);
+  i += 1;
+}
+
+console.log(`i = ${i} after while loop`);
